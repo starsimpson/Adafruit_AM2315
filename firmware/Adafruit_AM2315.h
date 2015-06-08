d3594948705a8b491c0cb22bed188071289fbd7d
Star Simpson <stars@mit.edu>
2015-06-08 00:17:56 -0700

    modifying includes to remove Arduino-related headers

--- firmware/Adafruit_AM2315.h
+++ firmware/Adafruit_AM2315.h
@@ -14,13 +14,6 @@
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
-#if (ARDUINO >= 100)
- #include "Arduino.h"
-#else
- #include "WProgram.h"
-#endif
-#include "Wire.h"
-
 #define AM2315_I2CADDR       0x5C
 #define AM2315_READREG       0x03
 
